4a71b35a44964d6fb307df080f285e1052c87896
Jaehoo-dev <54227178+Jaehoo-dev@users.noreply.github.com>
2023-03-02 22:28:56 +0900

    update

--- src/App.tsx
+++ src/App.tsx
@@ -68,16 +68,18 @@ export default function App() {
             throw new Error("결제위젯 인스턴스가 없습니다.");
           }
 
-          await paymentWidget.requestPayment({
-            orderId: "AD8aZDpbzXs4EQa-UkIX6",
-            orderName: "토스 티셔츠 외 2건",
-            customerName: "김토스",
-            customerEmail: "customer123@gmail.com",
-            // successUrl: 'http://localhost:5173/success',
-            // failUrl: 'http://localhost:5173/fail',
-          });
-
-          // 결제 성공 페이지로 이동
+          try {
+            await paymentWidget.requestPayment({
+              orderId: "AD8aZDpbzXs4EQa-UkIX6",
+              orderName: "토스 티셔츠 외 2건",
+              customerName: "김토스",
+              customerEmail: "customer123@gmail.com",
+              successUrl: "http://localhost:5173/success",
+              failUrl: "http://localhost:5173/fail",
+            });
+          } catch (error) {
+            // handle error
+          }
         }}
       >
         결제하기
