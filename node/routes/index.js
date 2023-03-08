6b34a38389e6564be5c2246a329a02188f7dff06
Jaehoo-dev <54227178+Jaehoo-dev@users.noreply.github.com>
2023-03-08 14:33:53 +0900

    setup base node sample

---
+++ node/routes/index.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var got = require("got");
+var uuid = require("uuid").v4;
+
+var router = express.Router();
+
+var secretKey = "test_ak_ZORzdMaqN3wQd5k6ygr5AkYXQGwy";
+
+router.get("/", function (req, res) {
+  res.render("index", {
+    title: "구매하기",
+    orderId: uuid(),
+    orderName: "토스 티셔츠",
+    price: 50000,
+    customerName: "김토스",
+  });
+});
+
+router.get("/success", function (req, res) {
+  got
+    .post("https://api.tosspayments.com/v1/payments/" + req.query.paymentKey, {
+      headers: {
+        Authorization:
+          "Basic " + Buffer.from(secretKey + ":").toString("base64"),
+        "Content-Type": "application/json",
+      },
+      json: {
+        orderId: req.query.orderId,
+        amount: req.query.amount,
+      },
+      responseType: "json",
+    })
+    .then(function (response) {
+      console.log(response.body);
+      // TODO: 구매 완료 비즈니스 로직 구현
+
+      res.render("success", {
+        title: "성공적으로 구매했습니다",
+        amount: response.body.totalAmount,
+      });
+    })
+    .catch(function (error) {
+      res.redirect(
+        `/fail?code=${error.response?.body?.code}&message=${error.response?.body?.message}`
+      );
+    });
+});
+
+router.get("/fail", function (req, res) {
+  res.render("fail", {
+    message: req.query.message,
+    code: req.query.code,
+  });
+});
+
+module.exports = router;
