867f3135c7903bca0cdd7e6c8f08333101d3e4de
Jiho Shin <ziho@toss.im>
2023-05-10 23:30:39 +0900

    feat: vue 샘플 추가

---
+++ vue/src/router/index.js
@@ -0,0 +1,26 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+const routes = [
+  {
+    path: "/",
+    name: "Home",
+    component: import("@/views/homePage.vue"),
+  },
+  {
+    path: "/success",
+    name: "Success",
+    component: import("@/views/successPage.vue"),
+  },
+  {
+    path: "/fail",
+    name: "Fail",
+    component: import("@/views/failPage.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
