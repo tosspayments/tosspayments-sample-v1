e7715097c4fe7ae03046f041ac16ffa690e4d0b0
Jiho Shin <ziho.dev@gmail.com>
2023-05-10 23:51:18 +0900

    feat: vue 샘플 추가 (#17)
    * feat: vue 샘플 추가
    
    * fix: dynamic router

---
+++ vue/README.md
@@ -0,0 +1,29 @@
+# 결제위젯 Vue 샘플 프로젝트
+
+결제위젯 JavaScript SDK를 이용해서 간단한 주문서 페이지를 만들어 보는 샘플 프로젝트입니다. 자세한 연동 방법과 결제 과정은 [공식 연동 문서](https://docs.tosspayments.com/guides/payment-widget/integration)에서 확인하세요.
+
+## 데모
+
+[데모](https://codesandbox.io/s/payment-widget-vue-sample-ph0nvp)
+
+## 시작하기
+
+먼저 이 레포지토리를 클론합니다.
+
+```
+$ git clone https://github.com/tosspayments/payment-widget-sample # 샘플 프로젝트 클론
+$ cd payment-widget-sample/vue
+```
+
+의존성 패키지를 다운로드하고 서버를 실행합니다.
+
+```
+npm install
+npm run serve
+```
+
+## 더 알아보기
+
+- 토스페이먼츠 개발 블로그 👉 [30분 안에 결제 페이지 개발하기 (ft. 결제위젯)](https://velog.io/@tosspayments/결제위젯으로-30분안에-결제-페이지-개발하기)
+
+- 토스페이먼츠 연동 문서 👉 [결제위젯 이해하기](https://docs.tosspayments.com/guides/payment-widget/overview) | [결제위젯 연동 문서](https://docs.tosspayments.com/guides/payment-widget/integration)
---
+++ vue/src/views/homePage.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>주문서</h1>
+    <div id="payment-method"></div>
+    <button @click="clickPaymentButton">결제하기</button>
+  </div>
+</template>
+
+<script>
+import { generateRandomString } from "../utils/util";
+
+const clientKey = "test_ck_D5GePWvyJnrK0W0k6q8gLzN97Eoq";
+const paymentWidget = PaymentWidget(clientKey, PaymentWidget.ANONYMOUS);
+
+export default {
+  mounted() {
+    paymentWidget.renderPaymentMethods("#payment-method", 50_000);
+  },
+  methods: {
+    clickPaymentButton() {
+      paymentWidget
+        .requestPayment({
+          orderId: generateRandomString(),
+          orderName: "리팩터링 2판 외 1권",
+          successUrl: window.location.origin + "/success",
+          failUrl: window.location.origin + "/fail",
+          customerEmail: "customer123@gmail.com", // 고객 이메일 (선택)
+          customerName: "김토스", // 고객 이름 (선택)
+        })
+        .then((v) => console.log(v));
+    },
+  },
+};
+</script>
