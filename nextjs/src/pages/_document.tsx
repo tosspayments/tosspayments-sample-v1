c895db5375c8369525cb4558972411450416a1fb
sybock <sybock@snu.ac.kr>
2023-11-08 12:20:18 +0900

    feat: nextjs

--- nextjs/src/pages/_document.tsx
+++ nextjs/src/pages/_document.tsx
@@ -5,7 +5,6 @@ export default function Document() {
     <Html lang="ko">
       <Head>
         <link rel="icon" href="https://static.toss.im/icons/png/4x/icon-toss-logo.png" />
-        <title>토스페이먼츠 샘플 프로젝트</title>
       </Head>
       <body>
         <Main />
