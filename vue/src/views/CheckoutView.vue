44ea47d0051c170eb2f86d5564f9b8b80dad68c5
sybock <sybock@snu.ac.kr>
2023-12-21 11:28:15 +0900

    feat: vue comments

--- vue/src/views/CheckoutView.vue
+++ vue/src/views/CheckoutView.vue
@@ -7,12 +7,15 @@
       <div id="agreement"></div>
       <!-- 쿠폰 체크박스 -->
       <div style="padding-left: 25px">
-        <div class="checkable typography--p"><label for="coupon-box" class="checkable__label typography--regular"><input @change="updateAmount" id="coupon-box" class="checkable__input" type="checkbox" aria-checked="true"><span class="checkable__label-text">5,000원 쿠폰 적용</span></label></div>
+        <div class="checkable typography--p">
+          <label for="coupon-box" class="checkable__label typography--regular"
+            ><input @change="updateAmount" id="coupon-box" class="checkable__input" type="checkbox" aria-checked="true" /><span class="checkable__label-text">5,000원 쿠폰 적용</span></label
+          >
+        </div>
       </div>
       <!-- 결제하기 버튼 -->
       <div class="result wrapper">
-        <button @click="requestPayment" class="button" id="payment-button"
-        style="margin-top:30px; ">결제하기</button>
+        <button @click="requestPayment" class="button" id="payment-button" style="margin-top: 30px">결제하기</button>
       </div>
     </div>
   </div>
@@ -20,16 +23,16 @@
 
 <script>
 import { loadPaymentWidget, ANONYMOUS } from "@tosspayments/payment-widget-sdk";
-import { nanoid } from 'nanoid';
+import { nanoid } from "nanoid";
 
 export default {
   data() {
     return {
       paymentWidget: null,
       paymentMethodWidget: null,
-      // TODO: clientKey는 개발자센터의 결제위젯 연동 키 > 클라이언트 키로 바꾸세요. 
+      // TODO: clientKey는 개발자센터의 결제위젯 연동 키 > 클라이언트 키로 바꾸세요.
+      // TODO: customerKey는 구매자와 1:1 관계로 무작위한 고유값을 생성하세요.
       clientKey: "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm",
-      // TODO: customerKey는 구매자와 1:1 관계로 무작위한 고유값을 생성하세요. 
       customerKey: nanoid(),
       amount: 50000,
     };
@@ -39,6 +42,8 @@ export default {
       try {
         if (this.paymentWidget) {
           // ------ '결제하기' 버튼 누르면 결제창 띄우기 ------
+          // 결제를 요청하기 전에 orderId, amount를 서버에 저장하세요.
+          // 결제 과정에서 악의적으로 결제 금액이 바뀌는 것을 확인하는 용도입니다.
           // @docs https://docs.tosspayments.com/reference/widget-sdk#requestpayment결제-정보
           await this.paymentWidget.requestPayment({
             orderId: nanoid(),
@@ -47,7 +52,7 @@ export default {
             customerEmail: "customer123@gmail.com",
             customerMobilePhone: "01012341234",
             successUrl: `${window.location.origin}/success`,
-            failUrl: `${window.location.origin}/fail`
+            failUrl: `${window.location.origin}/fail`,
           });
         }
       } catch (error) {
@@ -55,7 +60,7 @@ export default {
         console.error(error);
       }
     },
-    async updateAmount (){
+    async updateAmount() {
       const coupon = document.getElementById("coupon-box");
       // ------  결제 금액 업데이트 ------
       // @docs https://docs.tosspayments.com/reference/widget-sdk#updateamount결제-금액
@@ -64,24 +69,19 @@ export default {
       } else {
         this.paymentMethodWidget.updateAmount(this.amount);
       }
-    }
+    },
   },
   async mounted() {
     // ------  결제위젯 초기화 ------
     // @docs https://docs.tosspayments.com/reference/widget-sdk#sdk-설치-및-초기화
     this.paymentWidget = PaymentWidget(this.clientKey, ANONYMOUS);
 
-    // ------  결제위젯 렌더링 ------
+    // ------  결제 UI 렌더링 ------
     // @docs https://docs.tosspayments.com/reference/widget-sdk#renderpaymentmethods선택자-결제-금액-옵션
-    this.paymentMethodWidget = this.paymentWidget.renderPaymentMethods(
-        "#payment-method",
-        { value: this.amount },
-        { variantKey: "DEFAULT" }
-      );
-    // ------  이용약관 렌더링 ------
+    this.paymentMethodWidget = this.paymentWidget.renderPaymentMethods("#payment-method", { value: this.amount }, { variantKey: "DEFAULT" });
+    // ------  이용약관 UI 렌더링 ------
     // @docs https://docs.tosspayments.com/reference/widget-sdk#renderagreement선택자-옵션
     this.paymentWidget.renderAgreement("#agreement", { variantKey: "AGREEMENT" });
   },
-
 };
 </script>
