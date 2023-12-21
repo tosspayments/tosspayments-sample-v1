5c8d85aefe0b9876aa74c0b19f4bc0935ec4e948
sybock <sybock@snu.ac.kr>
2023-12-21 11:26:56 +0900

    feat: react comments

--- react/src/pages/Checkout.tsx
+++ react/src/pages/Checkout.tsx
@@ -1,9 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import {
-  PaymentWidgetInstance,
-  loadPaymentWidget,
-  ANONYMOUS,
-} from "@tosspayments/payment-widget-sdk";
+import { PaymentWidgetInstance, loadPaymentWidget, ANONYMOUS } from "@tosspayments/payment-widget-sdk";
 import { nanoid } from "nanoid";
 
 import "../App.css";
@@ -13,15 +9,14 @@ const selector = "#payment-widget";
 
 // TODO: clientKey는 개발자센터의 결제위젯 연동 키 > 클라이언트 키로 바꾸세요.
 // TODO: customerKey는 구매자와 1:1 관계로 무작위한 고유값을 생성하세요.
+// @docs https://docs.tosspayments.com/reference/widget-sdk#sdk-설치-및-초기화
 const clientKey = "test_gck_docs_Ovk5rk1EwkEbP0W43n07xlzm";
 const customerKey = nanoid();
 
 export function CheckoutPage() {
   const { data: paymentWidget } = usePaymentWidget(clientKey, customerKey);
   // const paymentWidget = usePaymentWidget(clientKey, ANONYMOUS); // 비회원 결제
-  const paymentMethodsWidgetRef = useRef<ReturnType<
-    PaymentWidgetInstance["renderPaymentMethods"]
-  > | null>(null);
+  const paymentMethodsWidgetRef = useRef<ReturnType<PaymentWidgetInstance["renderPaymentMethods"]> | null>(null);
   const [price, setPrice] = useState(50_000);
 
   useEffect(() => {
@@ -29,16 +24,12 @@ export function CheckoutPage() {
       return;
     }
 
-    // ------  결제위젯 렌더링 ------
+    // ------  결제 UI 렌더링 ------
     // @docs https://docs.tosspayments.com/reference/widget-sdk#renderpaymentmethods선택자-결제-금액-옵션
-    const paymentMethodsWidget = paymentWidget.renderPaymentMethods(
-      selector,
-      { value: price },
-      { variantKey: "DEFAULT" }
-    );
+    const paymentMethodsWidget = paymentWidget.renderPaymentMethods(selector, { value: price }, { variantKey: "DEFAULT" });
 
-    // ------  이용약관 렌더링 ------
-    // @docs https://docs.tosspayments.com/reference/widget-sdk#renderagreement선택자
+    // ------  이용약관 UI 렌더링 ------
+    // @docs https://docs.tosspayments.com/reference/widget-sdk#renderagreement선택자-옵션
     paymentWidget.renderAgreement("#agreement", { variantKey: "AGREEMENT" });
 
     paymentMethodsWidgetRef.current = paymentMethodsWidget;
@@ -63,19 +54,14 @@ export function CheckoutPage() {
         <div id="agreement" />
         <div style={{ paddingLeft: "24px" }}>
           <div className="checkable typography--p">
-            <label
-              htmlFor="coupon-box"
-              className="checkable__label typography--regular"
-            >
+            <label htmlFor="coupon-box" className="checkable__label typography--regular">
               <input
                 id="coupon-box"
                 className="checkable__input"
                 type="checkbox"
                 aria-checked="true"
                 onChange={(event) => {
-                  setPrice(
-                    event.target.checked ? price - 5_000 : price + 5_000
-                  );
+                  setPrice(event.target.checked ? price - 5_000 : price + 5_000);
                 }}
               />
               <span className="checkable__label-text">5,000원 쿠폰 적용</span>
@@ -87,6 +73,8 @@ export function CheckoutPage() {
             className="button"
             style={{ marginTop: "30px" }}
             onClick={async () => {
+              // TODO: 결제를 요청하기 전에 orderId, amount를 서버에 저장하세요.
+              // 결제 과정에서 악의적으로 결제 금액이 바뀌는 것을 확인하는 용도입니다.
               try {
                 // ------ '결제하기' 버튼 누르면 결제창 띄우기 ------
                 // @docs https://docs.tosspayments.com/reference/widget-sdk#requestpayment결제-정보
