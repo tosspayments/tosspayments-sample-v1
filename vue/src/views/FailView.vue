79692ae55ed79ec051b02b0dd6a98a73ce7f2940
sybock <sybock@snu.ac.kr>
2024-04-03 14:22:15 +0900

    feat: Vue 성공/실패 페이지 수정

--- vue/src/views/FailView.vue
+++ vue/src/views/FailView.vue
@@ -1,22 +1,19 @@
 <template>
-  <div class="result wrapper">
-  <div class="box_section">  
-    <h2 style="padding: 20px 0px 10px 0px">
-      <img
-        width="25"
-        src="https://static.toss.im/3d-emojis/u1F6A8-apng.png"
-      />
-      결제 실패
-    </h2>
-    <p>code = {{ this.$route.query.code }}</p>
-    <p>message = {{ this.$route.query.message }}</p>
-    <div class="result wrapper">
-        <button class="button" onclick="location.href='https://docs.tosspayments.com/guides/payment-widget/integration';"
-        style="margin-top:30px; margin-right: 10px">연동 문서</button>
-        <button class="button" onclick="location.href='https://discord.gg/A4fRFXQhRu';"
-        style="margin-top:30px;background-color: #e8f3ff;color:#1b64da ">실시간 문의</button>
-      </div>   
+  <div id="info" class="box_section" style="width: 600px">
+    <img style="width: 100px" src="https://static.toss.im/lotties/error-spot-no-loop-space-apng.png" />
+    <h2>결제를 실패했어요</h2>
+
+    <div class="p-grid typography--p" style="margin-top: 50px">
+      <div class="p-grid-col text--left"><b>에러메시지</b></div>
+      <div class="p-grid-col text--right" id="message">{{ this.$route.query.message }}</div>
+    </div>
+    <div class="p-grid typography--p" style="margin-top: 10px">
+      <div class="p-grid-col text--left"><b>에러코드</b></div>
+      <div class="p-grid-col text--right" id="code">{{ this.$route.query.code }}</div>
+    </div>
+    <div class="p-grid">
+      <button class="button p-grid-col5" onclick="location.href='https://docs.tosspayments.com/guides/payment/integration';">연동 문서</button>
+      <button class="button p-grid-col5" onclick="location.href='https://discord.gg/A4fRFXQhRu';" style="background-color: #e8f3ff; color: #1b64da">실시간 문의</button>
     </div>
   </div>
-
 </template>
